fb560faae14cb9f479ff4414bf3dcb2beffc8c40
Kinshuk <kinshukjuneja@gmail.com>
2017-02-21 21:52:12 -0500

    Added singup/login - Ask TA for pfuser subclass

--- Parse-Chat/LoginViewController.swift
+++ Parse-Chat/LoginViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    let user = PFUser()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
+    @IBAction func signUp(_ sender: UIButton) {
+        user.username = emailTextField.text
+        user.password = passwordTextField.text
+        
+        user.signUpInBackground {
+            (success, error) -> Void in
+            if let error = error {
+                if ((error as NSError).userInfo["error"] as? String) != nil {
+                    print("Error Couldn't login!")
+                    let alert = UIAlertController(title: "Error", message: "Error Signing Up!", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            } else {
+                // Hooray! Let them use the app now.
+                print("Signed up!");
+                
+            }
+        }
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        let username = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) {
+            (user: PFUser?, error: Error?) -> Void in
+            if let error = error {
+                if ((error as NSError).userInfo["error"] as? String) != nil {
+                    let alert = UIAlertController(title: "Error", message: "Error Loging In!", preferredStyle: UIAlertControllerStyle.alert)
+                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            } else {
+                // Hooray! Let them use the app now.
+                NSLog("Logged in!");
+            }
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
