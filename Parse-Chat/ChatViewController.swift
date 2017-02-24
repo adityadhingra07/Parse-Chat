8224aecb72eab1ec62bd10d924f6d4b20c75433d
Aditya Dhingra <adityadhingra07@gmail.com>
2017-02-23 19:30:55 -0500

    textfield added

--- Parse-Chat/ChatViewController.swift
+++ Parse-Chat/ChatViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 import Parse
+import NextGrowingTextView
 
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // OUTLETS
     @IBOutlet weak var chatTableView: UITableView!
+    @IBOutlet weak var growingTextView: NextGrowingTextView!
+    @IBOutlet weak var send: UIButton!
+    @IBOutlet weak var inputContainerView: UIView!
+    @IBOutlet weak var inputContainerViewBottom: NSLayoutConstraint!
+    
     
     // GLOBAL VARS
     var messages: [PFObject] = []
@@ -26,6 +32,18 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         getParseMessage()
         
         // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillShow(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatViewController.keyboardWillHide(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        self.growingTextView.layer.cornerRadius = 4
+        self.growingTextView.backgroundColor = UIColor(white: 0.9, alpha: 1)
+        self.growingTextView.textContainerInset = UIEdgeInsets(top: 16, left: 0, bottom: 4, right: 0)
+        self.growingTextView.placeholderAttributedText = NSAttributedString(string: "Write message ...",
+                                                                            attributes: [NSFontAttributeName: self.growingTextView.font!,
+                                                                                         NSForegroundColorAttributeName: UIColor.gray
+            ]
+        )
     }
 
     override func didReceiveMemoryWarning() {
@@ -64,6 +82,38 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         cell.messageLabel?.text = text
         return cell
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    @IBAction func handleSendButton(_ sender: AnyObject) {
+        self.growingTextView.text = ""
+        self.view.endEditing(true)
+    }
+    
+    
+    func keyboardWillHide(_ sender: Notification) {
+        if let userInfo = (sender as NSNotification).userInfo {
+            if let _ = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size.height {
+                //key point 0,
+                self.inputContainerViewBottom.constant =  0
+                //textViewBottomConstraint.constant = keyboardHeight
+                UIView.animate(withDuration: 0.25, animations: { () -> Void in self.view.layoutIfNeeded() })
+            }
+        }
+    }
+    func keyboardWillShow(_ sender: Notification) {
+        if let userInfo = (sender as NSNotification).userInfo {
+            if let keyboardHeight = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size.height {
+                self.inputContainerViewBottom.constant = keyboardHeight
+                UIView.animate(withDuration: 0.25, animations: { () -> Void in
+                    self.view.layoutIfNeeded()
+                })
+            }
+        }
     }
     
     /*
